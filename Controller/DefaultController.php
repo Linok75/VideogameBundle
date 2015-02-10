f6ca896b66bd3baa9863561f125930f875c4089b
Linok75 <aribola@iut.univ-paris8.fr>
2015-02-10 11:04:20 +0100

    Update DefaultController.php

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace linok\videogameBundle\Controller;
+namespace Linok\VideogameBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
