bf392f7ee8a66a6e1d5b69990b9648d0c8c501c8
Linok75 <aribola@iut.univ-paris8.fr>
2015-02-10 10:52:06 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace videogameBundle\DependencyInjection;
+namespace linok\videogameBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
