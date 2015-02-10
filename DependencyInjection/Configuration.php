bbccbacd454a413085e69055cadcba47f2980029
Linok75 <aribola@iut.univ-paris8.fr>
2015-02-10 10:25:23 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VideogameBundle\DependencyInjection;
+namespace videogameBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('da_proto');
+        $rootNode = $treeBuilder->root('videogame');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
